b5e50a80e1d3f9b24f12a643d345bebb7c7ed3cf
priyamsrivastava <priyam.srivastava_cs18@gla.ac.in>
2019-09-19 23:32:07 +0530

    made all the getter and setter and defined constructor

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,4 +1,37 @@
 package main;
 
 public class Book {
+    String BookName;
+    String AuthorName;
+    String ISBN_No;
+
+    public Book(String BookName,String AuthorName,String ISBN_No){
+        this.BookName=BookName;
+        this.AuthorName=AuthorName;
+        this.ISBN_No=ISBN_No;
+    }
+
+    public String getISBN_No() {
+        return ISBN_No;
+    }
+
+    public void setISBN_No(String ISBN_No) {
+        this.ISBN_No = ISBN_No;
+    }
+
+    public String getAuthorName() {
+        return AuthorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        AuthorName = authorName;
+    }
+
+    public String getBookName() {
+        return BookName;
+    }
+
+    public void setBookName(String bookName) {
+        BookName = bookName;
+    }
 }
